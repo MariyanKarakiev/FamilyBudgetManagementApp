374e64c79516829f04fca5d312ed87cf8ef57433
Mariyan Karakiev <mariqnkarakiev@gmail.com>
2022-11-25 09:25:51 +0200

    Created service for Budget class

---
+++ FamilyBudgetManagementApp/Services/BudgetService.cs
@@ -0,0 +1,76 @@
+﻿using FamilyBudgetManagementApp.Data;
+using FamilyBudgetManagementApp.Models;
+using FamilyBudgetManagementApp.Services.Contracts;
+using FamilyBudgetManagementApp.ViewModels;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.EntityFrameworkCore;
+
+namespace FamilyBudgetManagementApp.Services
+{
+    public class BudgetService : IBudgetService
+    {
+        private AppDbContext dbContext;
+
+        public BudgetService(AppDbContext _dbContext)
+        {
+            dbContext = _dbContext;
+        }
+
+        public async Task<BudgetViewModel> GetBudgetInfoForStats()
+        {
+            // TO DO: return some info for statistics
+            var budget = await dbContext.FindAsync<Budget>(1);
+
+            CheckIfBudgetIsNull(budget);
+
+            var model = new BudgetViewModel()
+            {
+                Id = 1,
+                Balance = budget.Balance
+            };
+
+            return new BudgetViewModel();
+        }
+
+        public async Task ChargeBudget(decimal amount)
+        {
+            if (amount == 0 || amount < 0)
+            {
+                return;
+            }
+
+            var budget = await dbContext.FindAsync<Budget>(1);
+
+            CheckIfBudgetIsNull(budget);
+
+            budget.Balance += amount;
+
+            dbContext.SaveChanges();
+        }
+
+        public async Task DischargeBudget(decimal amount)
+        {
+            if (amount == 0 || amount < 0)
+            {
+                return;
+            }
+
+            var budget = await dbContext.FindAsync<Budget>(1);
+
+            CheckIfBudgetIsNull(budget);
+
+            //check if < 0
+            budget.Balance -= amount;
+
+            dbContext.SaveChanges();
+        }
+
+        private void CheckIfBudgetIsNull(Budget ?budget)
+        {
+            if (budget == null)
+            {
+                throw new ArgumentNullException("No budget found!");
+            }
+        }
+    }
+}
---
+++ FamilyBudgetManagementApp/Services/Contracts/IBudgetService.cs
@@ -0,0 +1,12 @@
+﻿using FamilyBudgetManagementApp.Models;
+using FamilyBudgetManagementApp.ViewModels;
+
+namespace FamilyBudgetManagementApp.Services.Contracts
+{
+    public interface IBudgetService
+    {
+        public Task<BudgetViewModel> GetBudgetInfoForStats();
+        public Task ChargeBudget(decimal amount);
+        public Task DischargeBudget(decimal amount);
+    }
+}
