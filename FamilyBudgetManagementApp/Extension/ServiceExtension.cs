b9ee6b4cc529c0951217f8044d0201dd2b4ad435
Mariyan Karakiev <mariqnkarakiev@gmail.com>
2022-11-25 09:19:45 +0200

    Separated Service registration and Sql config from Program.cs

---
+++ FamilyBudgetManagementApp/Extension/ServiceExtension.cs
@@ -0,0 +1,28 @@
+﻿using FamilyBudgetManagementApp.Data;
+using FamilyBudgetManagementApp.Services;
+using FamilyBudgetManagementApp.Services.Contracts;
+using Microsoft.EntityFrameworkCore;
+
+namespace FamilyBudgetManagementApp.Extension
+{
+    public static class ServiceExtension
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddSingleton<BudgetService>();
+           
+            return services;
+        }
+
+        public static IServiceCollection AddApplicationDbContexts(this IServiceCollection services, IConfiguration config)
+        {
+            var connectionString = config.GetConnectionString("DefaultConnection");
+
+            services.AddDbContext<AppDbContext>(options =>
+                 options.UseSqlServer(connectionString));
+            services.AddDatabaseDeveloperPageExceptionFilter();
+
+            return services;
+        }
+    }
+}
