0702d4492bea3cc6d9de02865535ae8495d84b1f
Mariyan Karakiev <mariqnkarakiev@gmail.com>
2022-11-29 12:04:11 +0200

    Chart and transaction list added

---
+++ FamilyBudgetManagementApp/ViewModels/TransactionViewModel.cs
@@ -0,0 +1,38 @@
+﻿using FamilyBudgetApp.Data.Enums;
+using FamilyBudgetApp.Data.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace FamilyBudgetApp.ViewModels
+{
+    public class TransactionViewModel
+    {
+        public byte Id { get; set; }
+
+        [Required]
+        public decimal Amount { get; set; }
+
+        [Required]
+        public string Currency { get; set; }
+      
+        [Required]
+        public string Type { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+
+        [Required]
+        public DateTime CreatedOn { get; set; }
+
+        [Required]
+        public DateTime ReccursOn { get; set; }
+
+        [Required]
+        public ushort TimesReccuring { get; set; }
+
+        [Required]
+        public bool IsReccuring { get; set; }
+
+        public byte BudgetId { get; set; }
+    }
+}
